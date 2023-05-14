0b2d29d67130ef0b3e81adf467e88126f8c8e4d4
dariomavec <dario.mavec@gmail.com>
2023-05-14 19:33:00 +0930

    Update

--- netlify/functions/scraper.js
+++ netlify/functions/scraper.js
@@ -5,17 +5,11 @@ const fs = require('fs');
 exports.handler = async (event, context) => {
   try {
     // Replace with the URL of the website you want to scrape
-    const url = 'https://www.floatingsauna.com.au/';
+    const url = 'https://lakederbyfloatingsauna.as.me/api/scheduling/v1/availability/times?owner=45aadd5c&appointmentTypeId=14920908&calendarId=4082433&startDate=2023-05-15&timezone=Australia%2FHobart';
   
     // Send a GET request to the URL
     const response = await axios.get(url);
-
-    // Parse the HTML content using Cheerio
-    const $ = cheerio.load(response.data);
-    console.log($)
-
-    // Replace with the CSS selector of the element you want to scrape
-    const element = $('.h4');
+    return response;
 
     // Save the data to a file
     fs.writeFileSync('data.txt', element.text());
